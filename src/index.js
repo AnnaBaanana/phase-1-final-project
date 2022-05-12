60fccd4748eb1182f1e6bcee127339c5983a8ce4
Anna Shulyak <ourmac@Ours-iMac.fios-router.home>
2022-05-12 16:22:29 -0400

    adding fecth request to API and rendering NFT images onto a page

--- src/index.js
+++ src/index.js
@@ -4,12 +4,27 @@ function domLoaded() {
     document.addEventListener("DOMContentLoaded", () => {
         console.log("DOM Loaded")
         getNFT()
+        renderNFT()
     })
 }
 
 function getNFT() {
-    fetch(url).then(res => res.json()).then(data => console.log(data.assets))
+    const cardHolder = document.querySelector("#card-container")
+    console.log(cardHolder)
+    fetch(url).then(res => res.json()).then(data => {
+        console.log(data.assets)
+        data.assets.forEach(card => cardHolder.append(renderNFT(card)))
+    })
+}
 
+function renderNFT(card) {
+    const div = document.createElement('div')
+    div.textContent = card.name
+    const image = document.createElement('img')
+    image.src = card.image_url
+    image.className = "cards"
+    div.append(image)
+    return div
 }
 
 domLoaded()
